be1642db148224ec9f289e227e2cf324206703de
Lucas Soares <lucassfb@gmail.com>
2021-06-29 22:14:06 -0300

    add desafio02

---
+++ 29-06_Codelab_Classes/desafio02_cadastroJogadoresFutebol.py
@@ -0,0 +1,20 @@
+class Jogadores:
+    def __init__(self, nome, partidas, gols):
+        self.nome = nome
+        self.partidas = partidas
+        self.gols = gols
+
+    def MostrarDados(self):
+        return f'''
+Nome = {self.nome}
+Partidas = {self.partidas}
+Gols = {self.gols}   
+'''
+
+
+jogador1 = Jogadores("Cristiano Ronaldo", 3, 4)
+jogador2 = Jogadores("Messi", 4, 5)
+
+
+print(jogador1.MostrarDados())
+print(jogador2.MostrarDados())
