2a2a99dae3271d8a5d8fd1334ea1067e201db5ed
Lucas Soares <lucassfb@gmail.com>
2021-06-29 22:28:35 -0300

    correcao de espacamento

--- 29-06_Codelab_Classes/desafio02_cadastroJogadoresFutebol.py
+++ 29-06_Codelab_Classes/desafio02_cadastroJogadoresFutebol.py
@@ -11,10 +11,10 @@ Partidas = {self.partidas}
 Gols = {self.gols}   
 '''
 
+print("Aproveitamento dos jogadores:")
 
 jogador1 = Jogadores("Cristiano Ronaldo", 3, 4)
 jogador2 = Jogadores("Messi", 4, 5)
-
 
 print(jogador1.MostrarDados())
 print(jogador2.MostrarDados())
